c939a580a9e8f45468d0188739f90195bccbd9e6
Andrea Esposito <andexpox@gmail.com>
2021-06-10 18:20:32 +0200

    sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <div>
-        <h1>Movie List:</h1>
+    <h1>Movie List:</h1>
 
+    <div>
         <ul>
             @foreach ($movies as $movie)
                 <li>
